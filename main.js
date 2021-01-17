d987ea1680d62216c18923187eff44db2bfeea42
Kevin <kevinandres1250987@gmail.com>
2021-01-17 18:44:41 -0500

    incompleto

---
+++ main.js
@@ -0,0 +1,16 @@
+var nombre= "kevin";
+var altura= "165";
+
+var concatenacion = nombre + " " + altura;
+document.write(nombre);
+document.write(" " + altura);
+
+var datos = document.getElementById("datos");
+datos.innerHTML = concatenacion;
+
+var datos = document.getElementById("datos");
+datos.innerHTML = nombre + " " + nombre;
+
+// Var crea una variale //
+// document.write imprime una variable //
+// document.getElementById("") toma los datos del id de html //
